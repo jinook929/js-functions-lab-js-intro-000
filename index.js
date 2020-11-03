656976718674dd696db96ff18fb16f393fc538c8
JINOOK JUNG <jinook929@gmail.com>
2020-11-03 02:46:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holiday, name) {
+function holidayCountdown(days, holiday) {
   return `Happy ${holiday}, ${name}!`
 }
